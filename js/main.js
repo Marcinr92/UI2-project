ffcd29f84b1a2ad1902640fbd44b5365858e3802
Marcinr92 <martin.rutkowski@live.se>
2017-04-12 10:33:59 +0200

    Linked the files

---
+++ js/main.js
@@ -0,0 +1,6 @@
+alert("test");
+var canvas = document.createElement("canvas");
+var canvasContext = canvas.getContext("2d");
+canvas.width = 640;
+canvas.height =480;
+document.body.appendChild(canvas);
